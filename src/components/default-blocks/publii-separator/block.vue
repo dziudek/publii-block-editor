0ce89049ea73cf722a028a70b9555c00497cdacd
Tomasz Dziuda <dziudek@gmail.com>
2019-05-13 16:05:47 +0200

    Added options for separator - type selection

--- src/components/default-blocks/publii-separator/block.vue
+++ src/components/default-blocks/publii-separator/block.vue
@@ -1,11 +1,38 @@
 <template>
-  <div
-    class="publii-block-separator"
-    contenteditable="true"
-    @keyup="getFocusFromTab"
-    @paste="pastePlainText"
-    ref="block">
-    <hr />
+  <div>
+    <div
+      class="publii-block-separator"
+      contenteditable="true"
+      @keyup="getFocusFromTab"
+      @paste="pastePlainText"
+      ref="block">
+      <hr :class="config.type" />
+    </div>
+
+    <div
+      class="wrapper-ui-top-menu"
+      v-if="$parent.isSelected">
+      <button
+        :class="{ 'wrapper-ui-top-menu-button': true, 'is-active': this.config.type === 'long' }"
+        tabindex="-1"
+        @click.stop="setType('long')">—</button>
+      <button
+        :class="{ 'wrapper-ui-top-menu-button': true, 'is-active': this.config.type === 'medium' }"
+        tabindex="-1"
+        @click.stop="setType('medium')">–</button>
+      <button
+        :class="{ 'wrapper-ui-top-menu-button': true, 'is-active': this.config.type === 'short' }"
+        tabindex="-1"
+        @click.stop="setType('short')">‑</button>
+      <button
+        :class="{ 'wrapper-ui-top-menu-button': true, 'is-active': this.config.type === 'dots' }"
+        tabindex="-1"
+        @click.stop="setType('dots')">∙∙∙</button>
+      <button
+        :class="{ 'wrapper-ui-top-menu-button': true, 'is-active': this.config.type === 'dot' }"
+        tabindex="-1"
+        @click.stop="setType('dot')">•</button>
+    </div>
   </div>
 </template>
 
@@ -19,7 +46,9 @@ export default {
   ],
   data () {
     return {
-      config: {},
+      config: {
+        type: 'long'
+      },
       content: ''
     };
   },
@@ -42,6 +71,9 @@ export default {
         e.returnValue = false;
       }
     },
+    setType (type) {
+      this.config.type = type;
+    },
     save () {
       this.$bus.$emit('block-editor-save-block', {
         id: this.id,
@@ -62,5 +94,44 @@ export default {
   margin: 32px 0;
   outline: none;
   width: 100%;
+
+  hr {
+    margin: 5px auto;
+    position: relative;
+  }
+
+  hr.long {
+    width: 100%;
+  }
+
+  hr.medium {
+    width: 50%;
+  }
+
+  hr.short {
+    width: 25%;
+  }
+
+  hr.dots,
+  hr.dot {
+    border: none;
+    height: 12px;
+    margin: 0 auto;
+
+    &:before {
+      content: "* * *";
+      left: 50%;
+      line-height: 6px;
+      position: absolute;
+      top: 75%;
+      transform: translateX(-50%) translateY(-50%);
+    }
+  }
+
+  hr.dot {
+    &:before {
+      content: "*";
+    }
+  }
 }
 </style>
