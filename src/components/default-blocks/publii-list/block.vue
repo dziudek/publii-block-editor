2138f5a424f016d10c0e2d43865982f6a57282ed
Tomasz Dziuda <dziudek@gmail.com>
2019-05-13 16:16:30 +0200

    Added fix for list type changes

--- src/components/default-blocks/publii-list/block.vue
+++ src/components/default-blocks/publii-list/block.vue
@@ -1,12 +1,27 @@
 <template>
-  <component
-    :is="config.listType"
-    contenteditable="true"
-    @keyup="getFocusFromTab"
-    @paste="pastePlainText"
-    v-html="content"
-    ref="block"
-    class="publii-block-list" />
+  <div>
+    <component
+      :is="config.listType"
+      contenteditable="true"
+      @keyup="getFocusFromTab"
+      @paste="pastePlainText"
+      v-html="content"
+      ref="block"
+      class="publii-block-list" />
+
+    <div
+      class="wrapper-ui-top-menu"
+      v-if="$parent.isSelected">
+      <button
+        :class="{ 'wrapper-ui-top-menu-button': true, 'is-active': this.config.listType === 'ul' }"
+        tabindex="-1"
+        @click.stop="setListType('ul')">ul</button>
+      <button
+        :class="{ 'wrapper-ui-top-menu-button': true, 'is-active': this.config.listType === 'ol' }"
+        tabindex="-1"
+        @click.stop="setListType('ol')">ol</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -47,7 +62,13 @@ export default {
         e.returnValue = false;
       }
 
-      if (e.code === 'Enter' && this.$refs['block'].innerHTML.substr(-13) === '<li><br></li>') {
+      if (
+        e.code === 'Enter' &&
+        (
+          this.$refs['block'].innerHTML.substr(-13) === '<li><br></li>' ||
+          this.$refs['block'].innerHTML.substr(-9) === '<li></li>'
+        )
+      ) {
         this.$bus.$emit('block-editor-add-block', 'publii-paragraph', this.id);
         this.$refs['block'].innerHTML = this.$refs['block'].innerHTML.substr(0, this.$refs['block'].innerHTML.length - 13);
         e.returnValue = false;
@@ -72,6 +93,10 @@ export default {
         e.returnValue = false;
       }
       */
+    },
+    setListType (type) {
+      this.save();
+      this.config.listType = type;
     },
     save () {
       this.content = this.$refs['block'].innerHTML;
