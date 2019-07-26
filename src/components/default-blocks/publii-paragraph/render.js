7b5b351c4d58f249d3644795e2e3f87c61b1b397
Tomasz Dziuda <dziudek@gmail.com>
2019-07-26 18:43:43 +0200

    Initial work with rendering functions

---
+++ src/components/default-blocks/publii-paragraph/render.js
@@ -0,0 +1,9 @@
+function render (blockData) {
+  let id = blockData.config.id ? 'id="' + blockData.config.id + '"' : '';
+  let cssClasses = blockData.config.cssClasses ? 'class="' + blockData.config.cssClasses + '"' : '';
+  let html = `<p>${blockData.content}</p>`;
+
+  return html;
+};
+
+export default render;
---
+++ src/components/default-blocks/publii-separator/render.js
@@ -0,0 +1,9 @@
+function render (blockData) {
+  let id = blockData.config.id ? 'id="' + blockData.config.id + '"' : '';
+  let cssClasses = blockData.config.cssClasses ? 'class="' + blockData.config.cssClasses + '"' : '';
+  let html = `<hr />`;
+
+  return html;
+};
+
+export default render;
