6246a54e9f5d5317187ec7a240f515bbf0edd188
Tomasz Dziuda <dziudek@gmail.com>
2019-08-07 19:45:30 +0200

    #21 - improved render function for ToC block

--- src/components/default-blocks/publii-toc/render.js
+++ src/components/default-blocks/publii-toc/render.js
@@ -1,7 +1,14 @@
 function render (blockData) {
   let id = blockData.config.advanced.id ? ' id="' + blockData.config.advanced.id + '"' : '';
   let cssClasses = blockData.config.advanced.cssClasses ? ' class="' + blockData.config.cssClasses + '"' : '';
-  let html = ``;
+  let html = `
+  <div${id}${cssClasses}>
+    <h2>${blockData.content.title}</h2>
+    <div>
+      ${blockData.content.toc}
+    </div>
+  </div>
+  `;
 
   return html;
 };
