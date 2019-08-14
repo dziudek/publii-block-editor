1af426db9e421bfa2e437e9e50aa7f3afb0328eb
Tomasz Dziuda <dziudek@gmail.com>
2019-08-14 11:10:35 +0200

    #15 - improved image render function

--- src/components/default-blocks/publii-image/render.js
+++ src/components/default-blocks/publii-image/render.js
@@ -1,7 +1,43 @@
 function render (blockData) {
   let id = blockData.config.advanced.id ? ' id="' + blockData.config.advanced.id + '"' : '';
   let cssClasses = blockData.config.advanced.cssClasses ? ' class="' + blockData.config.cssClasses + '"' : '';
+  let caption = `<figcaption>${blockData.content.caption}</figcaption>`;
   let html = ``;
+
+  if (blockData.config.link.url !== '') {
+    let relNoFollow = '';
+    let targetBlank = '';
+
+    if (blockData.config.link.noFollow) {
+      relNoFollow = ' rel="nofollow"';
+    }
+
+    if (blockData.config.link.targetBlank) {
+      targetBlank = ' target="_blank"'
+    }
+
+    html = `
+    <figure${id}${cssClasses} data-align="${blockData.config.imageAlign}">
+      <a href="${blockData.config.link.url}"${relNoFollow}${targetBlank}>
+        <img
+          src="${blockData.content.image}"
+          height="${blockData.content.imageHeight}"
+          width="${blockData.content.imageWidth}"
+          alt="${blockData.content.alt}" />
+      </a>
+      ${caption}
+    </figure>`;
+  } else {
+    html = `
+    <figure${id}${cssClasses} data-align="${blockData.config.imageAlign}">
+      <img
+        src="${blockData.content.image}"
+        height="${blockData.content.imageHeight}"
+        width="${blockData.content.imageWidth}"
+        alt="${blockData.content.alt}" />
+      ${caption}
+    </figure>`;
+  }
 
   return html;
 };
