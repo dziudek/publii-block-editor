e74e2ec066084865db2f320dbdd7b199d10f8aa1
Tomasz Dziuda <dziudek@gmail.com>
2019-08-19 12:34:10 +0200

    #15 - updated output for the quote block

--- src/components/default-blocks/publii-quote/render.js
+++ src/components/default-blocks/publii-quote/render.js
@@ -1,11 +1,20 @@
 function render (blockData) {
   let id = blockData.config.advanced.id ? ' id="' + blockData.config.advanced.id + '"' : '';
   let cssClasses = blockData.config.advanced.cssClasses ? ' class="' + blockData.config.cssClasses + '"' : '';
-  let html = `
-  <blockquote${id}${cssClasses}>
-    <p>${blockData.content.text}</p>
-    <cite>${blockData.content.author}</cite>
-  </blockquote>`;
+  let html = '';
+
+  if (blockData.content.author.trim() !== '') {
+    html = `
+    <figure${id}${cssClasses}>
+      <blockquote>${blockData.content.text}</blockquote>
+      <figcaption>${blockData.content.author}</figcaption>
+    </figure>`;
+  } else {
+    html = `
+    <blockquote${id}${cssClasses}>
+      ${blockData.content.text}
+    </blockquote>`;
+  }
 
   return html;
 };
