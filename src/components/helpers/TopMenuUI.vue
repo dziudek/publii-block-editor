fe37de224aa8e3bbd8e3ced285d6b99f0caa27ab
Bob Mitro <bogmitro@gmail.com>
2019-08-22 15:11:00 +0200

    #49 UI Updated

--- src/components/helpers/TopMenuUI.vue
+++ src/components/helpers/TopMenuUI.vue
@@ -10,7 +10,7 @@
     <span
       v-if="conversions.length"
       class="wrapper-ui-top-menu-title wrapper-ui-top-menu-conversions">
-      Convert block:
+      Convert to:
       <span
         v-for="(conversion, index) of conversions"
         :key="'conversion-' + index"
@@ -18,7 +18,7 @@
         @click="makeConversion(conversion.type, conversion.convert)">
         <icon :name="conversion.icon" />
         <span class="wrapper-ui-top-menu-conversion-tooltip">
-           Convert to {{ conversion.name }}
+          {{ conversion.name }}
         </span>
       </span>
     </span>
@@ -79,23 +79,48 @@ export default {
     justify-content: center;
     padding: 0;
     position: relative;
-    width: 32px;
+    width: 38px;
+
+    & > svg {
+        fill: $block-editor-color-text-medium-dark !important;
+    }
+      
+       // hover effect
+    &::before {
+       content: "";
+       background: $block-editor-color-light-dark;
+       border-radius: 3px;
+       display: block;
+       left: 50%;
+       opacity: 0;
+       position: absolute;
+       height: 34px;
+       top: 50%;
+       transition: all .15s cubic-bezier(.4,0,.2,1);
+       transform: scale(.5) translate(-50%, -50%);
+       transform-origin: left top;
+       width: 34px;
+       z-index: -1;
+    }
 
     &-tooltip {
       background: $color-editor-color-light-medium;
       border-radius: $block-editor-form-input-border-radius;
       box-shadow: 0 2px 6px rgba(0, 0, 0, .16);
+      color: $block-editor-color-text;
       display: flex;
       flex-wrap: wrap;
+      font-size: 13px;
+      font-weight: normal;
       justify-content: center;
       height: auto;
-      left: -80px;
       opacity: 0;
       padding: 5px 0;
       pointer-events: none;
       position: absolute;
-      top: 30px;
-      width: 180px;
+      text-transform: none;
+      top: 34px;
+      width: 64px;
       z-index: 10;
 
       &:after {
@@ -109,7 +134,7 @@ export default {
         left: 50%;
         position: absolute;
         top: -12px;
-        transform: scale(.5);
+        transform: translateX(-50%);
         width: 12px;
       }
     }
@@ -120,27 +145,32 @@ export default {
       .wrapper-ui-top-menu-conversion-tooltip {
         animation: fadeIn .75s forwards;
         opacity: 1;
-        transform: scale(1);
+        transform: scale(1) translateX(-50%);
       }
 
       @keyframes fadeIn {
         0% {
             opacity: 0;
-            transform: scale(.5);
+            transform: scale(.5) translateX(-50%);
         }
 
         50% {
             opacity: 1;
-            transform: scale(1.05);
+            transform: scale(1.05) translateX(-50%);
         }
 
         70% {
-            transform: scale(.9);
+            transform: scale(.9) translateX(-50%);
         }
 
         100% {
-            transform: scale(1);
+            transform: scale(1) translateX(-50%);
         }
+      }
+        
+      &::before {
+         opacity: 1;
+         transform: scale(1) translate(-50%, -50%);
       }
     }
   }
