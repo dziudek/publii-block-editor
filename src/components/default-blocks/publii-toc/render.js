60e7199d9476e71f0a52dccc6c2becf113282def
Tomasz Dziuda <dziudek@gmail.com>
2020-01-08 10:34:52 +0100

    Added toc CSS class for the TOC block

--- src/components/default-blocks/publii-toc/render.js
+++ src/components/default-blocks/publii-toc/render.js
@@ -1,6 +1,7 @@
 function render (blockData) {
   let id = blockData.config.advanced.id ? ' id="' + blockData.config.advanced.id + '"' : '';
-  let cssClasses = blockData.config.advanced.cssClasses ? ' class="' + blockData.config.advanced.cssClasses + '"' : '';
+  let cssClasses = ['toc', blockData.config.advanced.cssClasses].filter(item => item && item.trim() !== '');
+  cssClasses = cssClasses.length ? ' class="' + cssClasses.join(' ') + '"' : '';
   let tocHeading = '';
 
   if (blockData.content.title.trim() !== '') {
