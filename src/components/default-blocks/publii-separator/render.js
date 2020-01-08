3ee2f81e4954aecc797f1c0dc3012529a55b481d
Tomasz Dziuda <dziudek@gmail.com>
2020-01-08 10:30:29 +0100

    Changed classes for the separator block

--- src/components/default-blocks/publii-separator/render.js
+++ src/components/default-blocks/publii-separator/render.js
@@ -1,7 +1,6 @@
 function render (blockData) {
   let id = blockData.config.advanced.id ? ' id="' + blockData.config.advanced.id + '"' : '';
-  let cssClasses = [blockData.config.advanced.cssClasses, 'separator-' + blockData.config.type];
-  cssClasses = cssClasses.filter(item => item && item.trim() !== '' && item !== 'separator-dots');
+  let cssClasses = ['separator', blockData.config.advanced.cssClasses, 'separator--' + blockData.config.type].filter(item => item && item.trim() !== '');
 
   if (cssClasses.length) {
     cssClasses = ' class="' + cssClasses.join(' ') + '"';
