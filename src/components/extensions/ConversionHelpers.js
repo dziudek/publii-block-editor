9a0972fc2fb66c887ba122e3bfcf3cd47cd7a413
Tomasz Dziuda <dziudek@gmail.com>
2020-01-26 21:33:52 +0100

    Fixed BR support during conversion to paragraphs

--- src/components/extensions/ConversionHelpers.js
+++ src/components/extensions/ConversionHelpers.js
@@ -1,8 +1,9 @@
 export default class ConversionHelpers {
-  stripTags (input) {
+  stripTags (input, saveLineBreaks = true) {
     let div = document.createElement('div');
-    div.innerHTML = input;
-    let output = div.innerText;
+    div.innerHTML = input.replace(/<br>/gmi, '[[[BR]]]').replace(/<br \/>/gmi, '[[[BR]]]').replace(/<br\/>/gmi, '[[[BR]]]');
+    // eslint-disable-next-line
+    let output = div.innerText.replace(/\[\[\[BR\]\]\]/gmi, "\n");
 
     return output;
   }
