0c5805506c9436c933a03f0cb618cd5521c1d4d4
Tomasz Dziuda <dziudek@gmail.com>
2020-03-06 00:23:52 +0100

    Fixed renderer function for the code block

--- src/components/default-blocks/publii-code/render.js
+++ src/components/default-blocks/publii-code/render.js
@@ -2,7 +2,7 @@ function render (blockData) {
   let id = blockData.config.advanced.id ? ' id="' + blockData.config.advanced.id + '"' : '';
   let languageClass = ' language-' + blockData.config.language;
   let cssClasses = ' class="' + blockData.config.advanced.cssClasses + languageClass + '"';
-  let html = `<pre${id}${cssClasses}><code>${blockData.content}</code></pre>`;
+  let html = `<pre${id}${cssClasses}><code>${blockData.content.replace(/</gmi, '&lt;').replace(/>/gmi, '&gt;')}</code></pre>`;
   return html;
 };
 
