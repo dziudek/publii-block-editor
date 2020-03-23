0c51ab63668c3a698d6022616943131b3d1fd8cf
Bob Mitro <bob@tidycustoms.net>
2020-03-23 20:51:48 +0100

    Addedd line numbers class to the pre element

--- src/components/default-blocks/publii-code/render.js
+++ src/components/default-blocks/publii-code/render.js
@@ -1,7 +1,7 @@
 function render (blockData) {
   let id = blockData.config.advanced.id ? ' id="' + blockData.config.advanced.id + '"' : '';
-  let languageClass = ' language-' + blockData.config.language;
-  let cssClasses = ' class="' + blockData.config.advanced.cssClasses + languageClass + '"';
+  let languageClass = 'language-' + blockData.config.language;
+  let cssClasses = ' class="line-numbers ' + blockData.config.advanced.cssClasses + languageClass + '"';
   let html = `<pre${id}${cssClasses}><code>${blockData.content.replace(/</gmi, '&lt;').replace(/>/gmi, '&gt;')}</code></pre>`;
   return html;
 };
